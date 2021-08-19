a0973828a8d25f6db1cb4625909eafd4f8e59ae6
mrjackmaster <jacektrg+github@gmail.com>
2021-08-19 16:29:14 +0200

    Very basic app. Handles changes to input value state, submitting task to array and deleting task.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Overview from './components/Overview';
 
 function App() {
-	return <div></div>;
+	const [inputValue, setInputValue] = useState('');
+	const [myArray, setMyArray] = useState([]);
+
+	function handleChange(e) {
+		setInputValue((prevState) => e.target.value);
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		setMyArray((prevState) => [
+			...prevState,
+			{ title: inputValue, id: myArray.length },
+		]);
+		setInputValue((prevState) => '');
+	}
+
+	function handleDelete(e) {
+		const id = Number(e.target.parentElement.id);
+		setMyArray((prevState) =>
+			prevState.filter((item) => {
+				return item.id !== id;
+			})
+		);
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label>
+					Task title
+					<input
+						type='text'
+						name='task'
+						placeholder='Your task'
+						onChange={handleChange}
+						value={inputValue}
+					/>
+				</label>
+				<button>Add Task</button>
+			</form>
+			<Overview
+				data={myArray}
+				handleDelete={handleDelete}
+				handleSubmit={handleSubmit}
+			/>
+		</div>
+	);
 }
 
 export default App;
